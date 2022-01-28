7552ff08b78632481f8bd07dce0aad8070eb447b
Silviu Ifrim <silviu.redent@gmail.com>
2022-01-28 22:22:25 +0200

    Register users and trips endpoints

--- TravelCompanion/api/urls.py
+++ TravelCompanion/api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from .views.trip import TripViewSet
+from .views.user import UserViewSet
+
 app_name = 'api'
 
 router = routers.DefaultRouter()
 
 # routes will go here
-
+router.register('trips', TripViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
