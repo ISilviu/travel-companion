43e70000bb22a1d2a80a0b9f3f322c0059035173
Silviu Ifrim <silviu.redent@gmail.com>
2022-01-29 08:50:42 +0200

    Add read/write serializers for trips

--- TravelCompanion/api/serializers/trip.py
+++ TravelCompanion/api/serializers/trip.py
@@ -1,4 +1,8 @@
 from rest_framework import serializers
+
+from .city import CitySerializer
+from .user import UserTripSerializer
+
 from ..models.trip import Trip, TripCity
 
 
@@ -8,8 +12,18 @@ class TripCitySerializer(serializers.ModelSerializer):
         fields = ['trip', 'city', 'flight_number']
 
 
-class TripSerializer(serializers.ModelSerializer):
+class ReadonlyTripSerializer(serializers.ModelSerializer):
+    initiator = UserTripSerializer(read_only=True)
+    participants = UserTripSerializer(many=True, read_only=True)
+    cities = CitySerializer(many=True, read_only=True)
+
     class Meta:
         model = Trip
         fields = ['initiator', 'participants',
                   'cities', 'start_date', 'end_date', 'price']
+
+
+class TripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = ReadonlyTripSerializer.Meta.fields
--- TravelCompanion/api/views/trip.py
+++ TravelCompanion/api/views/trip.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 
 from ..models.trip import Trip
-from ..serializers.trip import TripSerializer
+from ..serializers.trip import ReadonlyTripSerializer, TripSerializer
 
 
 class TripViewSet(viewsets.ModelViewSet):
-    queryset = Trip.objects.select_related('initiator').prefetch_related('participants', 'cities')
-    serializer_class = TripSerializer
+    queryset = Trip.objects.select_related(
+        'initiator').prefetch_related('participants', 'cities')
+
+    def get_serializer_class(self):
+        is_read_action = self.action == 'list' or self.action == 'retrieve'
+        return ReadonlyTripSerializer if is_read_action else TripSerializer
