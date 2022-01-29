44a91288bf76a76bb48a0e7ef6a76cf350241240
Silviu Ifrim <silviu.redent@gmail.com>
2022-01-29 20:24:36 +0200

    Add tests for no results

--- TravelCompanion/api/test_views/mixins.py
+++ TravelCompanion/api/test_views/mixins.py
@@ -1,6 +1,6 @@
 from ddf import G
 from django.db import models
-from rest_framework import serializers 
+from rest_framework import serializers, status
 
 class ReadonlyOperationsTestsMixin:
     test_model: models.Model = None
@@ -14,6 +14,11 @@ class ReadonlyOperationsTestsMixin:
         self.assertEqual(result.data, self.serializer_class(
             self.test_model.objects.all(), many=True).data)
 
+    def test_list_no_results(self):
+        result = self.client.get(self.url_base)
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(result.data), 0)
+
     def test_retrieve(self):
         id = 5
         G(self.test_model, id=id)
@@ -24,3 +29,9 @@ class ReadonlyOperationsTestsMixin:
         expected = self.serializer_class(
             self.test_model.objects.filter(id=id).first()).data
         self.assertEqual(result.data, expected)
+
+    def test_retrieve_not_found(self):
+        result = self.client.get(f'{self.url_base}5/')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+
+    
