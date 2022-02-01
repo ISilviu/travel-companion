c6100bb79c5839acfe55c6d32986a0517fe8f18f
Silviu Ifrim <silviu.redent@gmail.com>
2022-02-01 16:38:58 +0200

    Add models comments.

--- TravelCompanion/api/models/trip.py
+++ TravelCompanion/api/models/trip.py
@@ -4,6 +4,14 @@ from .city import City
 
 
 class Trip(models.Model):
+    """
+    The trip model, consisting of the following fields:
+    - initiator - the user that initiated the trip
+    - participants - the other users that joined the trip
+    - cities - the cities (destinations) of the trip
+    - start_date, end_date - the begin and end dates of the trip
+    - price - the total price of the trip
+    """
     initiator = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='initiated_trips')
     participants = models.ManyToManyField(
@@ -15,6 +23,10 @@ class Trip(models.Model):
 
 
 class TripCity(models.Model):
+    """
+    The join entity between Trip and City. 
+    Contains the flight number as an extra field, could be extended to include further details like reservations.
+    """
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     flight_number = models.CharField(max_length=50)
--- TravelCompanion/api/models/user.py
+++ TravelCompanion/api/models/user.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    """
+    Even though django provides a default user model, it was overridden to allow further customizations.
+    """
     pass
