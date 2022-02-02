0965a1ccb803d61bbb6b994fcb5d5ab2b2e25d88
Silviu Ifrim <silviu.redent@gmail.com>
2022-02-02 16:05:34 +0200

    Add user register tests.

--- TravelCompanion/api/test_views/user.py
+++ TravelCompanion/api/test_views/user.py
@@ -1,4 +1,4 @@
-
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 from .mixins import CommonOperationsMixin
@@ -14,3 +14,16 @@ class UserApiTests(CommonOperationsMixin, APITestCase):
     test_model = User
     serializer_class = UserSerializer
     url_base = '/api/users/'
+
+    def test_create_no_password(self):
+        response = self.client.post(self.url_base, data={'username': 'isilviu'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_successful(self):
+        response = self.client.post(self.url_base, data={'username': 'isilviu', 'password': 'hello'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_auth_token(self):
+        pass
+
+
