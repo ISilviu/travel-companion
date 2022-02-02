2f1ef21ac7f470190d0ef02fb8457985bb94152d
Silviu Ifrim <silviu.redent@gmail.com>
2022-02-02 16:48:14 +0200

    Secure /users endpoint

--- TravelCompanion/api/views/user.py
+++ TravelCompanion/api/views/user.py
@@ -1,15 +1,20 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import permissions
 
 from ..serializers.user import ReadonlyInitiatedTripsSerializer, UserSerializer
 from ..models.user import User
 
+class CreateOnlyPermission(permissions.IsAuthenticated):
+    def has_permission(self, request, view):
+        return True if view.action == 'create' else super().has_permission(request, view)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.prefetch_related(
         'participating_trips', 'initiated_trips')
     serializer_class = UserSerializer
+    permission_classes = [CreateOnlyPermission]
 
     @action(detail=True)
     def trips(self, request, pk):
