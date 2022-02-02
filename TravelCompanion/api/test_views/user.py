cb5255696d22786a600ed50bd5d04b3ab7a1716f
Silviu Ifrim <silviu.redent@gmail.com>
2022-02-02 16:16:48 +0200

    Add authentication tests

--- TravelCompanion/api/test_views/user.py
+++ TravelCompanion/api/test_views/user.py
@@ -16,14 +16,33 @@ class UserApiTests(CommonOperationsMixin, APITestCase):
     url_base = '/api/users/'
 
     def test_create_no_password(self):
-        response = self.client.post(self.url_base, data={'username': 'isilviu'}, format='json')
+        response = self.client.post(
+            self.url_base, data={'username': 'isilviu'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_successful(self):
-        response = self.client.post(self.url_base, data={'username': 'isilviu', 'password': 'hello'}, format='json')
+        response = self.client.post(self.url_base, data={
+                                    'username': 'isilviu', 'password': 'hello'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def _retrieve_auth_token(self):
+        user_data = {
+            'username': 'isilviu',
+            'password': 'hello'
+        }
+        self.client.post(self.url_base, data=user_data, format='json')
+        response = self.client.post('/auth/', data=user_data, format='json')
+        return response.data['token']
+
     def test_get_auth_token(self):
-        pass
+        token = self._retrieve_auth_token()
+        self.assertTrue(len(token))
 
+    def test_access_secured_route(self):
+        response = self.client.get('/api/trips/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+        token = self._retrieve_auth_token()
+        response = self.client.get(
+            '/api/trips/', format='json', **{'HTTP_AUTHORIZATION': f'Token {token}'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
