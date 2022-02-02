15d5360f477e94f4b43d32a147a8a8665c1b9b55
Silviu Ifrim <silviu.redent@gmail.com>
2022-02-02 16:25:09 +0200

    Secure /cities endpoint.

--- TravelCompanion/api/views/city.py
+++ TravelCompanion/api/views/city.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from ..models.city import City
 from ..serializers.city import CitySerializer
@@ -7,3 +8,4 @@ from ..serializers.city import CitySerializer
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+    permission_classes = [permissions.IsAuthenticated]
